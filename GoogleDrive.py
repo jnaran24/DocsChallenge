052a135365680ed22b79cc12de8fcfa6c55929bd
Camilo <jnaran24@eafit.edu.co>
2022-06-27 18:15:35 -0500

    La bd se crea desde la app, se hace constrain de la key nombre_archivo, creación de tabla de historico con documentos que fueron publicos en alguna corrida

--- GoogleDrive.py
+++ GoogleDrive.py
@@ -4,10 +4,12 @@ from Google import Create_Service
 import base64
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+import psycopg2
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
 directorio_credenciales = 'credentials_module.json'
 
-#INICIAR SESION
+# INICIAR SESION
 def login():
     gauth = GoogleAuth()
     gauth.LoadCredentialsFile(directorio_credenciales)
@@ -20,59 +22,153 @@ def login():
     
     return GoogleDrive(gauth)
 
-#CREAR ARCHIVO DE TEXTO SIMPLE
-def buscar():
-    resultado = []
-    credenciales = login() #Nos autenticamos
-    # Se realiza el query para obtener los archivos de la carpeta contenedora
-    lista_archivos = credenciales.ListFile({'q': "'1cY1CM7x-K0XovdcxRI7sPuAzMcmsTPzE' in parents and trashed=false"}).GetList()
-    for f in lista_archivos:
+# METODO EJECUTOR
+def insertDataBase():
+    # ----------------------CONEXIÓN A LA BASE DE DATOS SQL--------------------------------
+    try:        
+        connection=psycopg2.connect(
+            user ='postgres', 
+            host = 'localhost', 
+            password = '123456789', 
+            port=5432) # Datos basicos para conexión con la BD
+        connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT) # Autocommit a las sentencias ejecutadas
+        cursor = connection.cursor() # Cursor de la BD principal
+
+        try:
+            cursor.execute('CREATE DATABASE melichallenge') # Creamos la BD
+        except:
+            print("La base de datos ya se encuentra creada")
+            pass
         
-        print('Nombre deel archivo:', f['title']) #Nombre del archivo
-        tipo = f['title'] #Tipo del archivo
-        print('Tipo archivo:', tipo.split(".",1)[1])
-        print('ultima modificación:', f['modifiedDate']) #Fecha de modificación    
-        atributos = f.metadata
-        infoPropietario = atributos["owners"]
-        for variable in infoPropietario:
-            print("owner:", variable["displayName"]) #Nombre del owner y email 
-            print("email:", variable["emailAddress"])
+        cursor.execute('SELECT current_database()') # Brindamos información de la base de datos principal
+        for row in cursor.fetchall():
+            print('the main database is: ',row)
+        print('')
+        cursor.close() # Cerramos cursos y conexión de la base de datos principal
+        connection.close()
         
+        connection2=psycopg2.connect( # Nos conectamos a la BD que creamos
+            user ='postgres', 
+            database='melichallenge', 
+            host = 'localhost', 
+            password = '123456789', 
+            port=5432)
+        cursor2 = connection2.cursor() # Cursor de la nueva BD
+        connection2.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT) # Autocommit a las sentencias ejecutadas
 
-        if(f['shared'] == True):
-            CLIENTE = "client_secrets.json"
-            API_NAME = "gmail"
-            API_VERSION = "v1"
-            SCOPES = ["https://mail.google.com/"]
+        try: # Intentamos crear las tablas, principal y historicos
+            cursor2.execute('''CREATE TABLE principal(
+                nombre_archivo VARCHAR,
+                extension_archivo VARCHAR(5),
+                propietario VARCHAR,
+                visibilidad VARCHAR(8),
+                fecha_modificacion DATE
+            );
+            ALTER TABLE principal ADD UNIQUE (nombre_archivo)''')
+            cursor2.execute('''CREATE TABLE historicos(
+                nombre_archivo VARCHAR,
+                extension_archivo VARCHAR(5),
+                propietario VARCHAR,
+                visibilidad VARCHAR(8),
+                fecha_modificacion DATE
+            );
+            ALTER TABLE principal ADD UNIQUE (nombre_archivo)''')
+        except:
+            print("las tablas que intenta crear ya existen")
+            pass
 
-            service = Create_Service(CLIENTE, API_NAME, API_VERSION, SCOPES)
-
-            mimeMessage = MIMEMultipart()
-            mimeMessage["subject"] = "Notificación cambio de privacidad Google Drive"
-            nombreArchivo = f['title']
-            emailMsg = "Cordial saludo, este es un mensaje automatico para notificarte que el archivo {0} en Google Drive se encontraba con acceso publico y se ha cambiado a restringido".format(nombreArchivo)
-            mimeMessage["to"] = variable["emailAddress"]
+        resultado = []
+        credenciales = login() #Nos autenticamos
+        # Se realiza el query para obtener los archivos de la carpeta contenedora
+        lista_archivos = credenciales.ListFile({'q': "'1cY1CM7x-K0XovdcxRI7sPuAzMcmsTPzE' in parents and trashed=false"}).GetList()
+        for f in lista_archivos:
+            nombre = f['title'] # Nombre del archivo
+            extension = nombre.split(".",1)[1] # Extension del archivo
+            visibilidad = f['shared']
+            fechaUltimaMod = f['modifiedDate']
             
-            mimeMessage.attach(MIMEText(emailMsg, "plain"))
-            raw_string = base64.urlsafe_b64encode(mimeMessage.as_bytes()).decode()
-            message = service.users().messages().send(userId = "me", body = {"raw": raw_string}).execute()
-            print(message)
-            gauth = GoogleAuth()
-            drive = GoogleDrive(gauth) # authorization script
-            file = drive.CreateFile({'id': f['id']}) # Se reemplaza el archivo existente (update)
-            permission_list = file.GetPermissions() # Se obtienen los permisos del archivo
-            for obj in permission_list: # Ciclo que recorre los permisos
-                for variable in infoPropietario: # Visitamos la data del owner
-                    if obj.get('emailAddress') != variable["emailAddress"]: # Si el email visitante no concuerda con el owner
-                        file.DeletePermission(obj['id']) # Se restringe el permiso 
-                        
+            print('Nombre deel archivo:', nombre) #Nombre del archivo            
+            print('Tipo archivo:', extension)
+            print('ultima modificación:', fechaUltimaMod) #Fecha de modificación    
+            atributos = f.metadata # Asignamos la metadata a una variable
+            infoPropietario = atributos["owners"] # Asignamos la info del owner a una variable
+            for variable in infoPropietario: # Recorremos esa info del owner
+                print("owner:", variable["displayName"]) #Nombre del owner y email 
+                print("email:", variable["emailAddress"])
 
-        resultado.append(f)
+                try: # Archivo por archivo intentamos ingresar su información
+                    cursor2.execute('''INSERT INTO principal (nombre_archivo,
+                    extension_archivo,
+                    propietario,
+                    visibilidad,
+                    fecha_modificacion)
+                    VALUES('{0}','{1}','{2}','{3}','{4}');'''.format(
+                        nombre,
+                        extension,
+                        variable["displayName"],
+                        visibilidad,
+                        fechaUltimaMod
+                    ))
+                except Exception as ex:
+                    print(ex)
+                    pass            
 
-    return resultado
+            if(visibilidad == True): #True si visibilidad es publica, False si es privada
+                #-----------SE ALMACENA EL REGISTRO EN TABLA HISTORICOS--------------------
+                try:
+                    cursor2.execute('''INSERT INTO historicos (nombre_archivo,
+                    extension_archivo,
+                    propietario,
+                    visibilidad,
+                    fecha_modificacion)
+                    VALUES('{0}','{1}','{2}','{3}','{4}');'''.format(
+                        nombre,
+                        extension,
+                        variable["displayName"],
+                        visibilidad,
+                        fechaUltimaMod
+                    ))
+                except Exception as ex:
+                    print(ex)
+                    pass
+
+                #----------------SERVICIO DE EMAIL PARA NOTIFICACION-----------------------
+                CLIENTE = "client_secrets.json"
+                API_NAME = "gmail"
+                API_VERSION = "v1"
+                SCOPES = ["https://mail.google.com/"]
+
+                service = Create_Service(CLIENTE, API_NAME, API_VERSION, SCOPES)
+                mimeMessage = MIMEMultipart()
+                mimeMessage["subject"] = "Notificación cambio de privacidad archivo de Google Drive"
+                emailMsg = "Cordial saludo, este es un mensaje automatico para notificarte que el archivo {0} en Google Drive se encontraba con acceso publico y se ha cambiado a restringido".format(nombre)
+                mimeMessage["to"] = variable["emailAddress"]                
+                mimeMessage.attach(MIMEText(emailMsg, "plain"))
+                raw_string = base64.urlsafe_b64encode(mimeMessage.as_bytes()).decode()
+                message = service.users().messages().send(userId = "me", body = {"raw": raw_string}).execute()
+                print(message)
+
+                #---------------------SE ASIGNA VISIBILIDAD PRIVADA--------------------------
+                gauth = GoogleAuth()
+                drive = GoogleDrive(gauth) # authorization script
+                file = drive.CreateFile({'id': f['id']}) # Se reemplaza el archivo existente (update)
+                permission_list = file.GetPermissions() # Se obtienen los permisos del archivo
+                for obj in permission_list: # Ciclo que recorre los permisos
+                    for variable in infoPropietario: # Visitamos la data del owner
+                        if obj.get('emailAddress') != variable["emailAddress"]: # Si el email visitante no concuerda con el owner
+                            file.DeletePermission(obj['id']) # Se restringe el permiso                             
+
+            resultado.append(f)
+        return resultado        
+    except Exception as ex:
+        print(ex)
+    finally:
+        cursor2.close()
+        connection2.close() #Se cierra la conexión a la BD
+        print("Conexión finalizada")
 
 
 
 
 if __name__ == "__main__":
-    buscar()
+    insertDataBase()
